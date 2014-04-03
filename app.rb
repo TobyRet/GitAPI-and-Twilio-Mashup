78ded7291494f8d866439de686aeba7c31b7676a
Toby Retallick <toby.retallick@gmail.com>
2014-04-03 13:16:44 +0100

    Added comments

--- app.rb
+++ app.rb
@@ -4,12 +4,21 @@ require 'json'
 require 'net/http'
 
 post '/message' do
-  # username_url = "https://api.github.com/users/#{params[:Body]}"
-  user = Octokit.user params[:Body]
-
+  # username_url = "https://api.github.com/users/#{params[:Body]}
   # resp = Net::HTTP.get_response(URI.parse(username_url))
   # result = JSON.parse(resp.body)
-  # p result
+
+  # short cut for the above
+  user = Octokit.user params[:Body]
+
+  content_type 'text/xml' 
+  "<Response>
+    <Message>#{user.name} is on GitHub and has #{user.followers} followers!</Message>
+  </Response>"
+end
+
+post '/call' do
+  user = Octokit.user params[:Body]
 
   content_type 'text/xml' 
   "<Response>
